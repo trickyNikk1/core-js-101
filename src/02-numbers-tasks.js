1b1123f04ee3c1869ee6c169883cf11964099c8b
Nikita Aristarkhov <aristarhovnikita1@gmail.com>
2023-10-18 13:13:28 +0300

    pass 16 tests

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -86,7 +86,9 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(a, b) {}
+function getLinearEquationRoot(/* a, b */) {
+  throw new Error('Not implemented');
+}
 
 /**
  * Returns an angle (in radians) between two vectors given by xi and yi,
